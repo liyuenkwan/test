270c72546e4b2ecb265029ae66d812903aabde80
Stephen Li <stephenli@thisisnoble.com>
2016-04-22 18:08:44 +0800

    no message

--- src/main/java/com/ngl/App.java
+++ src/main/java/com/ngl/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         try {
-            System.out.println( "Hello World!" );
+            System.out.println( "Hello World!!!!" );
         } catch (Exception e) {
             e.printStackTrace();
         }
